9263902988c506e423633c2ce8dbfa5c473bd313
Nmrhame <65970649+Nmrhame@users.noreply.github.com>
2022-05-16 11:47:33 -0400

    Firction & speed controls

--- car.js
+++ car.js
@@ -4,15 +4,19 @@ class Car{
         this.y = y;
         this.width = width;
         this.height = height;
+        this.speed = 0;
+        this.acceralation = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
         this.control = new Control();
     }
 
     update(){
         if(this.control.forward){
-            this.y -= 2;
+            this.speed += this.acceralation;
         }
         if(this.control.reverse){
-            this.y += 2;
+            this.speed -= this.acceralation;
         }
         if(this.control.left){
             this.x -= 2;
@@ -20,6 +24,19 @@ class Car{
         if(this.control.right){
             this.x += 2;
         }
+        if(this.speed > this.maxSpeed){
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed < -this.maxSpeed){
+            this.speed = -this.maxSpeed/2;
+        }
+        if(this.speed > 0){
+            this.speed -= this.friction;
+        }
+        if(this.speed < 0){
+            this.speed += this.friction;
+        }  
+        this.y -= this.speed;
     }
 
     draw(context){
