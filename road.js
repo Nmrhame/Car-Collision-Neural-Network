fb2522ffaf43e47a75e1ec4d2c8ff8a68a2d6fce
Nmrhame <65970649+Nmrhame@users.noreply.github.com>
2022-05-16 12:35:07 -0400

    Road Generation

--- road.js
+++ road.js
@@ -6,22 +6,31 @@ class Road{
         this.left = x - width/2;
         this.right = x + width/2;
         const infinity = 1000000;
-        this.top = infinity;
+        this.top = -infinity;
         this.bottom = infinity;
+    }
+
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width/this.laneCount;
+        return this.left+laneWidth/2+Math.min(laneIndex,this.laneCount-1)*laneWidth;
     }
 
     draw(context){
         this.lineWidth = 5;
         context.strokeStyle = "white";
-        
-        context.beginPath();
-        context.moveTo(this.left, this.top);
-        context.lineTo(this.left, this.bottom);
-        context.stroke();
 
-        context.beginPath();
-        context.moveTo(this.right, this.top);
-        context.lineTo(this.right, this.bottom);
-        context.stroke();
+        for(let i = 0; i < this.laneCount+1; i++){
+            const x = lerp(this.left, this.right, i/this.laneCount);
+            if(i>0 && i<this.laneCount){
+                context.setLineDash([20,20]);
+            }
+            else{
+                context.setLineDash([]);
+            }
+            context.beginPath();
+            context.moveTo(x, this.top);
+            context.lineTo(x, this.bottom);
+            context.stroke();
+        }
     }
 }
